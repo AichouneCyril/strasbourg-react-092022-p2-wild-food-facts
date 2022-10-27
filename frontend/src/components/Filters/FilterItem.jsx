1e3e51333fb30fc058eb889ba8a9cea59830d00f
Geoffrey Hach <je.suis.geoffrey.hach@gmail.com>
2022-10-27 10:36:56 +0200

    added filterslist and filteritem component

---
+++ frontend/src/components/Filters/FilterItem.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Box, Radio, Typography } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+
+function FilterItem({ itemName }) {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <Box display="flex" alignItems="center">
+      {active ? (
+        <CheckCircleIcon onClick={handleClick} color="primary" />
+      ) : (
+        <RadioButtonUncheckedIcon onClick={handleClick} color="primary" />
+      )}
+      <Typography sx={{ mx: 1, my: 0.2 }}>{itemName}</Typography>
+    </Box>
+  );
+}
+
+FilterItem.propTypes = {
+  itemName: PropTypes.string.isRequired,
+};
+
+export default FilterItem;
