6513b09ea717cfef05af77dfbbaec0b1272025be
AichouneCyril <aichoune.cyril@gmail.com>
2022-10-28 15:05:06 +0200

    Fonction SearchBar opérationnelle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,60 @@
+/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
+import React, { useState } from "react";
 import { ThemeProvider } from "@mui/material";
 import appTheme from "./assets/theme/theme";
-import PageCard from "./components/PageCard";
-
+import SearchBar from "./components/SearchBar";
 import "./App.css";
 
+const dataFood = [
+  { name: "Nutella", details: { score: "D", origin: "United-Sate" } },
+  { name: "Coca-Cola", details: { score: "D", origin: "United-Sate" } },
+  {
+    name: "100% mie complet-Harry's",
+    details: { score: "A", origin: "United-Sate" },
+  },
+  {
+    name: "Nocciolata-Rigoni di Asiago",
+    details: { score: "D", origin: "Italie" },
+  },
+  { name: "Skyr-Danone", details: { score: "A", origin: "France" } },
+];
+
+function Item({ name, details }) {
+  return (
+    <p>
+      {name} |neutri_score {details.score} | {details.origin}
+    </p>
+  );
+}
+
+function SearchResult({ newList }) {
+  return (
+    <>
+      {newList.map((item, index) => (
+        // eslint-disable-next-line react/no-array-index-key, react/jsx-props-no-spreading
+        <Item key={index} {...item} />
+      ))}
+    </>
+  );
+}
+
 function App() {
+  const [data, setData] = useState(dataFood);
   return (
     <ThemeProvider theme={appTheme}>
       <div className="App">
-        <PageCard>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quia id
-          explicabo minima delectus aut nemo quo unde laborum nisi aspernatur in
-          officiis hic odit deleniti, mollitia rerum. Fugiat vel est culpa
-          voluptates maxime eveniet fuga animi possimus aliquid. In architecto
-          quas hic itaque reiciendis voluptatem recusandae fugiat voluptate
-          dicta veritatis magni voluptatibus magnam, corrupti corporis. Eveniet
-          similique, quaerat ad aut doloribus dicta est suscipit, illo maxime,
-          delectus fugit molestias optio illum error sunt perspiciatis quam hic
-          voluptatem cum id. Id aspernatur vel, hic praesentium atque quis ex
-          voluptatibus officiis quas. Odit omnis commodi qui iure non distinctio
-          id autem.
-        </PageCard>
+        <SearchBar setList={setData} list={data} dataFood={dataFood} />
+        <SearchResult newList={data} />
       </div>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+Item.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  name: PropTypes.node.isRequired,
+  details: PropTypes.node.isRequired,
+};
