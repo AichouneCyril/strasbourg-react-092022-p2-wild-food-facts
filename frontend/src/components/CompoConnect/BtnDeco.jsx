dab381701e39dde359f1c6c4f23ad81868fd032a
louis nowak <nowak.louis@gmail.com>
2022-11-06 18:52:15 +0100

    up connect 2

--- frontend/src/components/CompoConnect/BtnDeco.jsx
+++ frontend/src/components/CompoConnect/BtnDeco.jsx
@@ -6,7 +6,7 @@ import Button from "@mui/material/Button";
 export default function BtnDeco() {
   return (
     <Stack spacing={2} direction="row" sx={{ display: "flex", justifyContent: "center", paddingTop: "15vh"}}>
-      <Button variant="contained" sx={{width: "180px", backgroundColor:"white", borderRadius: "50px", boxShadow: "3px 5px 0px 0px grey"}}>Déconnection</Button>
+      <Button variant="contained" sx={{width: "180px", backgroundColor:"secondary.contrastText", borderRadius: "50px", boxShadow: "3px 5px 0px 0px grey"}}>Déconnection</Button>
     </Stack>
   );
 }
--- frontend/src/components/CompoConnect/SupprUser.jsx
+++ frontend/src/components/CompoConnect/SupprUser.jsx
@@ -6,7 +6,7 @@ import Button from "@mui/material/Button";
 export default function BtnDeco() {
   return (
     <Stack spacing={2} direction="row" sx={{ display: "flex", justifyContent: "center", paddingTop: "5vh"}}>
-      <Button variant="contained" sx={{width: "180px", color: "red", backgroundColor:"white", borderRadius: "50px", boxShadow: "3px 5px 0px 0px grey"}}>Supprimer Votre Comptre</Button>
+      <Button variant="contained" sx={{width: "180px", color: "red", backgroundColor:"secondary.contrastText", borderRadius: "50px", boxShadow: "3px 5px 0px 0px grey"}}>Supprimer Votre Comptre</Button>
     </Stack>
   );
 }
