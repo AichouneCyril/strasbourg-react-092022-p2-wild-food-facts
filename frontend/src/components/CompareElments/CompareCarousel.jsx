61c8d97e7c1eec462d889298702371c394b6f77b
Rosecore67 <cr67.rose@gmail.com>
2022-11-10 10:48:23 +0100

    Suppression page test

--- frontend/src/components/CompareElments/CompareCarousel.jsx
+++ frontend/src/components/CompareElments/CompareCarousel.jsx
@@ -14,48 +14,50 @@ import Box from "@mui/material/Box";
 
 function CarouselCompare({ product }) {
   return (
-    <Stack
-      direction="row"
-      spacing={3}
-      style={{ overflowX: "scroll", height: "100%", width: "100%" }}
-    >
-      {[...Array(24).keys()].map((value) => (
-        <Card
-          key={value}
-          item
-          style={{ width: "100%", minWidth: "310px" }}
-          className="DisplayItem"
-        >
-          <CardHeader
-            title={product.generic_name}
-            variant="h1"
-            sx={{ textAlign: "center" }}
-          />
-          <IconButton aria-label="add to favorites" sx={{}}>
-            <FavoriteIcon />
-          </IconButton>
-          <CardMedia
-            sx={{ objectFit: "contain" }}
-            height="200px"
-            component="img"
-            image={product.image_url}
-            alt={product.generic_name}
-          />
+    product && (
+      <Stack
+        direction="row"
+        spacing={3}
+        style={{ overflowX: "scroll", height: "100%", width: "100%" }}
+      >
+        {[...Array(24).keys()].map((value) => (
+          <Card
+            key={value}
+            item
+            style={{ width: "100%", minWidth: "310px" }}
+            className="DisplayItem"
+          >
+            <CardHeader
+              title={product.generic_name}
+              variant="h1"
+              sx={{ textAlign: "center" }}
+            />
+            <IconButton aria-label="add to favorites" sx={{}}>
+              <FavoriteIcon />
+            </IconButton>
+            <CardMedia
+              sx={{ objectFit: "contain" }}
+              height="200px"
+              component="img"
+              image={product.image_url}
+              alt={product.generic_name}
+            />
 
-          <CardContent>
-            <Box>
-              <Stack direction="row" spacing={2}>
-                <Button variant="text">
-                  Nutri score: {product.nutriscore_grade}
-                </Button>
-                <Button>Eco score: {product.ecoscore_grade}</Button>
-                <Button>Nova score: {product.nova_group}</Button>
-              </Stack>
-            </Box>
-          </CardContent>
-        </Card>
-      ))}
-    </Stack>
+            <CardContent>
+              <Box>
+                <Stack direction="row" spacing={2}>
+                  <Button variant="text">
+                    Nutri score: {product.nutriscore_grade}
+                  </Button>
+                  <Button>Eco score: {product.ecoscore_grade}</Button>
+                  <Button>Nova score: {product.nova_group}</Button>
+                </Stack>
+              </Box>
+            </CardContent>
+          </Card>
+        ))}
+      </Stack>
+    )
   );
 }
 
