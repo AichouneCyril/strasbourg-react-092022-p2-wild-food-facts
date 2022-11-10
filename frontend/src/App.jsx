dc411304a5b3c314d47ff0812f07d27c735dbba7
Rosecore67 <cr67.rose@gmail.com>
2022-11-10 12:15:33 +0100

    version finale

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,16 @@
 import React, { useState } from "react";
-// import axios from "axios";
-/* import SearchBar from "./components/SearchBar"; */
-// import FiltersList from "./components/Filters/FiltersList";
 import PageCard from "./components/PageCard";
 import Navbar from "./components/Navbar";
 import ThemeProvider from "./theme";
 import Home from "./pages/Home";
 import "./App.css";
 import SearchResultsPage from "./pages/SearchResultsPage";
-// import CompareCarousel from "./components/CompareElments/CompareCarousel";
 
 function App() {
   const [menu, setMenu] = useState("home");
   const [open, setOpen] = useState(true);
-  // const [product, setProduct] = useState({});
   const [query, setQuery] = useState("");
   const [data, setData] = useState();
-  /* const [data, setData] = useState(dataFood); */
-
-  // useEffect(() => {
-  //   axios
-  //     .get("https://world.openfoodfacts.org/api/v0/product/737628064502.json")
-  //     .then((response) => response.data)
-  //     .then((data) => {
-  //       console.warn(data.product);
-  //       setProduct(data.product);
-  //     });
-  // }, []);
-
-  // console.warn(product);
 
   return (
     <ThemeProvider>
@@ -40,9 +22,6 @@ function App() {
           query={query}
           setQuery={setQuery}
         />
-        {/* <PageCard>
-          <FiltersList />
-        </PageCard> */}
         <Navbar changeMenu={setMenu} changeOpen={setOpen} />
         {menu && menu !== "home" && (
           <PageCard changeMenu={setMenu} etatOpen={open} changeOpen={setOpen}>
